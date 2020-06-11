cb2a7aa5612d435600550f5cd786d6dcc949e65e
mekhan <margub.khan@gmail.com>
2020-06-11 10:19:52 +0530

    Initial commit

---
+++ src/test/java/com/margub/vendor/VendorwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.margub.vendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
