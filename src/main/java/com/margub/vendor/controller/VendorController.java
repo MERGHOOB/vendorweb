404bf7bfdb8efbbcae1b16d6e22dbf1342378647
mekhan <margub.khan@gmail.com>
2020-06-11 16:18:35 +0530

    add  view all fucntiaonlity displayVendors.jsp Controller update for that VendorController.java added to travel using jstl in displayVendors.jsp addVendor, deleteVendor, edit vendor functionality added

--- src/main/java/com/margub/vendor/controller/VendorController.java
+++ src/main/java/com/margub/vendor/controller/VendorController.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.persistence.Id;
+import java.util.List;
 
 @Controller
 public class VendorController {
@@ -30,4 +32,34 @@ public class VendorController {
         return "createVendor";
     }
 
+    @RequestMapping("/displayVendors")
+    public String displayVendors(ModelMap modelMap) {
+        List<Vendor> allVendors = vendorService.getAllVendors();
+        modelMap.addAttribute("vendors", allVendors);
+        return "displayVendors";
+    }
+
+    @RequestMapping("/updateVendor")
+    public String updateVendor(@RequestParam("id")int id, ModelMap modelMap) {
+        Vendor vendorById = vendorService.getVendorById(id);
+        modelMap.addAttribute("vendor", vendorById);
+        return "updateVendor";
+    }
+
+    @RequestMapping("/saveUpdate")
+    public String updateVendor(@ModelAttribute Vendor vendor, ModelMap modelMap) {
+        vendorService.updateVendor(vendor);
+
+        List<Vendor> allVendors = vendorService.getAllVendors();
+        modelMap.addAttribute("vendors", allVendors);
+        return "displayVendors";
+    }
+
+    @RequestMapping("/deleteVendor")
+    public String deleteVendor(@RequestParam("id") int id, ModelMap modelMap) {
+        vendorService.deleteVendor(vendorService.getVendorById(id));
+        List<Vendor> allVendors = vendorService.getAllVendors();
+        modelMap.addAttribute("vendors", allVendors);
+        return "displayVendors";
+    }
 }
